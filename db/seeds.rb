563cd8e6b0c2c3559a938eaa1be0a2a0e4a46f25
Blake Schlin <blakeschlin@gmail.com>
2018-03-06 19:04:03 -0600

    recreated seed file products with .save

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,5 +8,8 @@
 
 
 product = Product.new({name: "Tide Pods", price: 15, image_url: "https://www.google.com/", description: "Cleaning product"})
+product.save
 product = Product.new({name: "Paper Towels", price: 10, image_url: "https://en.wikipedia.org/wiki/Paper_towel", description: "Kitchen product"})
+product.save
 product = Product.new({name: "Kleenex", price: 5, image_url: "https://www.merriam-webster.com/dictionary/toiletry", description: "Toiletry product"})
+product.save
