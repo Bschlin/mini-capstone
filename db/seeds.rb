8501742bfba0d781f0e8b48c81705c641a83c56b
Blake Schlin <blakeschlin@gmail.com>
2018-03-06 18:53:36 -0600

    created product in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+product = Product.new({name: "Tide Pods", price: 15, image_url: "https://www.google.com/", description: "Cleaning product"})
+product = Product.new({name: "Paper Towels", price: 10, image_url: "https://en.wikipedia.org/wiki/Paper_towel", description: "Kitchen product"})
+product = Product.new({name: "Kleenex", price: 5, image_url: "https://www.merriam-webster.com/dictionary/toiletry", description: "Toiletry product"})
