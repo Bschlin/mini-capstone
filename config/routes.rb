c47dc6bca5bbfd625ddea8e03e5e4e862ad1814d
Blake Schlin <blakeschlin@gmail.com>
2018-03-06 19:46:57 -0600

    added multiple routes for individual products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   get "/all_products" => "products#all_products"
+   get "/first_product_method" => "products#first_product_method"
+   get "/second_product_method" => "products#second_product_method"
+   get "/third_product_method" => "products#third_product_method"
 end
--- product_list.rb
+++ product_list.rb
@@ -1,11 +1,30 @@
 require "unirest"
 
 system "clear"
-puts "Here are your products!"
+puts "Choose an option"
+puts "[1] Product 1"
+puts "[2] Product 2"
+puts "[3] Product 3"
 
- response = Unirest.get("http://localhost:3000/all_products")
+
+input_option = gets.chomp
+if input_option == "1"
+  response = Unirest.get("http://localhost:3000/first_product_method")
   product = response.body
   puts JSON.pretty_generate(product)
+elsif input_option == "2"
+  response = Unirest.get("http://localhost:3000/second_product_method")
+  product = response.body
+   puts JSON.pretty_generate(product)
+elsif input_option == "3"
+  response = Unirest.get("http://localhost:3000/third_product_method")
+  product = response.body
+  puts JSON.pretty_generate(product)
+end
+
+ # response = Unirest.get("http://localhost:3000/all_products")
+ #  product = response.body
+ #  puts JSON.pretty_generate(product)
 
 
 # input_option = gets.chomp
@@ -22,3 +41,5 @@ puts "Here are your products!"
 #   product = response.body
 #   puts JSON.pretty_generate(product)
 # end
+
+
