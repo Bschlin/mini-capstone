0759c096bdf9e52aeb037b9f7aae7ee042d68ebe
Blake Schlin <blakeschlin@gmail.com>
2018-03-12 21:03:33 -0500

    model methods added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,29 @@
 class Product < ApplicationRecord
+
+  def is_discounted
+    price <= 2
+  end 
+
+  def tax
+    price * 0.09
+  end 
+
+  def total
+    # price * 0.09 + price
+    price + tax
+  end 
+
+  def as_json
+    { 
+      id: id,
+      name: name,
+      price: price,
+      image_url: image_url,
+      description: description,
+      tax: tax,
+      total: total,
+      discounted: is_discounted,
+    }
+  end
+
 end
