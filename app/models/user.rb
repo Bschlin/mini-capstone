21464b7427be30937fa95949359d9776b907e7b7
Blake Schlin <blakeschlin@gmail.com>
2018-03-24 14:44:43 -0500

    add associations to wire up all of the models to each other

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-    has_secure_password
-    validates :name, presence: true
-    validates :email, presence: true, uniqueness: true
+  has_secure_password
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 
-    has_many :orders
+  has_many :orders
+  has_many :carted_products
 end
